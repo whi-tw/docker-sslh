b73d6aaaa5f8cde50edb9add247f700de6f46f80
Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>
2019-06-06 21:46:00 +0100

    Replace --ssl flag with --tls
    --ssl has been deprecated, and replaced with --tls. Updating to
    future-proof this docker image

--- entry.sh
+++ entry.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 SSLH_OPTS=
 [ ! -z "${SSH_HOST}" ] && SSLH_OPTS="${SSLH_OPTS} --ssh ${SSH_HOST}:${SSH_PORT:=22}"
-[ ! -z "${HTTPS_HOST}" ] && SSLH_OPTS="${SSLH_OPTS} --ssl ${HTTPS_HOST}:${HTTPS_PORT:=443}"
+[ ! -z "${HTTPS_HOST}" ] && SSLH_OPTS="${SSLH_OPTS} --tls ${HTTPS_HOST}:${HTTPS_PORT:=443}"
 [ ! -z "${OPENVPN_HOST}" ] && SSLH_OPTS="${SSLH_OPTS} --openvpn ${OPENVPN_HOST}:${OPENVPN_PORT:=1194}"
 [ ! -z "${SHADOWSOCKS_HOST}" ] && SSLH_OPTS="${SSLH_OPTS} --anyprot ${SHADOWSOCKS_HOST}:${SHADOWSOCKS_PORT:=8388}"
 
